845d85f5f1370cc54d4f8e3997a4f06b3af24dda
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-04-03 16:14:33 +0100

    CME-126 add db monitoring action group

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -77,3 +77,14 @@ variable "additional_databases" {
   default = []
 }
 
+variable "db_monitor_action_group_name" {
+  description = "The name of the Action Group to create."
+  type        = string
+  default     = "db_monitor_ag"
+}
+
+variable "db_alert_email_address_key" {
+  description = "Email address key in azure Key Vault."
+  type        = string
+  default     = "db-alert-monitoring-email-address"
+}
