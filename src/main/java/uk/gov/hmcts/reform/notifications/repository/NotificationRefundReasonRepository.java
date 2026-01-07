978fa6ab962b9673353f52a082620fe5837cd35f
Dave Jones <38425925+davejones74@users.noreply.github.com>
2026-01-07 14:57:45 +0000

    Revert "Pay it feature (#663)"
    This reverts commit ed424c4612233909c6b9b831d90f46bc3007f9de.

--- src/main/java/uk/gov/hmcts/reform/notifications/repository/NotificationRefundReasonRepository.java
+++ src/main/java/uk/gov/hmcts/reform/notifications/repository/NotificationRefundReasonRepository.java
@@ -3,13 +3,9 @@ package uk.gov.hmcts.reform.notifications.repository;
 import org.springframework.data.repository.CrudRepository;
 import uk.gov.hmcts.reform.notifications.model.NotificationRefundReasons;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface NotificationRefundReasonRepository extends CrudRepository<NotificationRefundReasons, String> {
 
     Optional<NotificationRefundReasons> findByRefundReasonCode(String code);
-
-    // Convenience method to return all rows as a List
-    List<NotificationRefundReasons> findAll();
 }
